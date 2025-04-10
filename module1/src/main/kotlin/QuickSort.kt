292a767e8ec5f8921ad74531d8cc96365778c980
Youssef Ahmed <joetifa2003@gmail.com>
2025-04-10 02:45:32 +0200

    fix: quick sort
    Only to make the test pass for GH actions.
    But tests are working!

--- module1/src/main/kotlin/QuickSort.kt
+++ module1/src/main/kotlin/QuickSort.kt
@@ -7,12 +7,10 @@ fun <T : Comparable<T>> quickSort(items: List<T>): List<T> {
     }
     val pivot = items[items.size / 2]
 
- // val equal = items.filter { it == pivot }
+    val equal = items.filter { it == pivot }
     val less = items.filter { it < pivot }
     val greater = items.filter { it > pivot }
-
- // return quickSort(less) + equal + quickSort(greater)
-    return quickSort(less) + quickSort(greater)
+    return quickSort(less) + equal + quickSort(greater)
 }
 
 fun main() {
